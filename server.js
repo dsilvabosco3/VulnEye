6ddeee5d7ac11502ae06b206249b713722044263
dsilvabosco3 <dsilvabosco3@gmail.com>
2026-03-17 21:10:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -574,4 +574,11 @@ app.get("/api/video-scans", async (req, res) => {
 /* Start server */
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("Server running on port", PORT);
+});
+
+// health check (keep server alive)
+app.get("/health", (req, res) => {
+  res.send("OK");
+});
