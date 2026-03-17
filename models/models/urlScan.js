146a617f13078e6c1b04268a6441eed1b2e92fed
dsilvabosco3 <dsilvabosco3@gmail.com>
2026-03-17 08:41:39 +0530

    Create urlScan.js

---
+++ models/models/urlScan.js
@@ -0,0 +1,15 @@
+module.exports = (conn) => {
+  const mongoose = require("mongoose");
+
+  const urlScanSchema = new mongoose.Schema({
+  target: String,
+  type: String,
+  severity: String,
+  userId: String,
+  username: String,
+  timestamp: { type: Date, default: Date.now }
+});
+
+
+  return conn.model("UrlScan", urlScanSchema, "urlscans");
+};
