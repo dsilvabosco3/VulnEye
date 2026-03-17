a28449a2458a5733abbfebb2e459526aa6ced8a4
dsilvabosco3 <dsilvabosco3@gmail.com>
2026-03-17 21:35:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -53,12 +53,10 @@ const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
   secure: false,
+  family: 4,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
-  },
-  tls: {
-    rejectUnauthorized: false
   }
 });
 transporter.verify(function (error, success) {
