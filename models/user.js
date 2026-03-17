8894eeda38d76137c040c77f92251d70dca89204
dsilvabosco3 <dsilvabosco3@gmail.com>
2026-03-17 19:31:01 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,12 +1,10 @@
-module.exports = (conn) => {
-  const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
-  const userSchema = new mongoose.Schema({
-    username: { type: String, required: true },
-    email: String,
-    password: { type: String, required: true },
-    createdAt: { type: Date, default: Date.now }
-  });
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  email: String,
+  password: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now }
+});
 
-  return conn.model("User", userSchema, "users");
-};
+module.exports = mongoose.model("User", userSchema);
