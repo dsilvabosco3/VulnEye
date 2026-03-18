fbb6c38712d581cf959cd0a4e5b0832dbd2149ef
dsilvabosco3 <dsilvabosco3@gmail.com>
2026-03-18 11:18:43 +0530

    Update urlScan.js

--- models/urlScan.js
+++ models/urlScan.js
@@ -1,12 +1,14 @@
-const mongoose = require("mongoose");
+module.exports = (conn) => {
+  const mongoose = require("mongoose");
 
-const urlScanSchema = new mongoose.Schema({
-  target: String,
-  type: String,
-  severity: String,
-  userId: String,
-  username: String,
-  timestamp: { type: Date, default: Date.now }
-});
+  const urlScanSchema = new mongoose.Schema({
+    target: String,
+    type: String,
+    severity: String,
+    userId: String,
+    username: String,
+    timestamp: { type: Date, default: Date.now }
+  });
 
-module.exports = mongoose.model("UrlScan", urlScanSchema);
+  return conn.model("UrlScan", urlScanSchema, "urlscans");
+};
