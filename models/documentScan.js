28d5a082f4b980293eb9bc117ec66b7143e8eff2
dsilvabosco3 <dsilvabosco3@gmail.com>
2026-03-18 11:14:31 +0530

    Update documentScan.js

--- models/documentScan.js
+++ models/documentScan.js
@@ -1,12 +1,14 @@
-const mongoose = require("mongoose");
+module.exports = (conn) => {
+  const mongoose = require("mongoose");
 
-const documentScanSchema = new mongoose.Schema({
-  target: String,
-  type: String,
-  severity: String,
-  userId: String,
-  username: String,
-  timestamp: { type: Date, default: Date.now }
-});
+  const documentScanSchema = new mongoose.Schema({
+    target: String,
+    type: String,
+    severity: String,
+    userId: String,
+    username: String,
+    timestamp: { type: Date, default: Date.now }
+  });
 
-module.exports = mongoose.model("DocumentScan", documentScanSchema);
+  return conn.model("DocumentScan", documentScanSchema, "documentscans");
+};
