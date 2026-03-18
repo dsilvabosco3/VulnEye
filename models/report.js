850ffbd1bd9fec643cf35481755aefcc37cbe3a4
dsilvabosco3 <dsilvabosco3@gmail.com>
2026-03-18 11:19:57 +0530

    Update report.js

--- models/report.js
+++ models/report.js
@@ -1,10 +1,12 @@
-const mongoose = require("mongoose");
+module.exports = (conn) => {
+  const mongoose = require("mongoose");
 
-const reportSchema = new mongoose.Schema({
-  scanType: String,
-  scanId: String,
-  reportData: Object,
-  createdAt: { type: Date, default: Date.now }
-});
+  const reportSchema = new mongoose.Schema({
+    scanType: String,
+    scanId: String,
+    reportData: Object,
+    createdAt: { type: Date, default: Date.now }
+  });
 
-module.exports = mongoose.model("Report", reportSchema);
+  return conn.model("Report", reportSchema, "reports");
+};
