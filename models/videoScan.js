a90b82815b0ef942c4a87b3cf90e9534e2c5930f
dsilvabosco3 <dsilvabosco3@gmail.com>
2026-03-18 11:19:32 +0530

    Update videoScan.js

--- models/videoScan.js
+++ models/videoScan.js
@@ -1,12 +1,14 @@
-const mongoose = require("mongoose");
+module.exports = (conn) => {
+  const mongoose = require("mongoose");
 
-const videoScanSchema = new mongoose.Schema({
-  target: String,
-  type: String,
-  severity: String,
-  userId: String,
-  username: String,
-  timestamp: { type: Date, default: Date.now }
-});
+  const videoScanSchema = new mongoose.Schema({
+    target: String,
+    type: String,
+    severity: String,
+    userId: String,
+    username: String,
+    timestamp: { type: Date, default: Date.now }
+  });
 
-module.exports = mongoose.model("VideoScan", videoScanSchema);
+  return conn.model("VideoScan", videoScanSchema, "videoscans");
+};
