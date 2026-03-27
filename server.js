f9929e5f1e6bcc00feb14033d666f7f18d96b82d
dsilvabosco3 <dsilvabosco3@gmail.com>
2026-03-27 08:11:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
 
-
 const path = require("path");
 
 app.use(express.static(path.join(__dirname, "public")));
